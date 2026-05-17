f0583c313035fe08f13ae649c02efc5d5823a71c
Shade666 <rsr@revarch.co.uk>
2026-05-17 20:09:53 +0100

    feat(02-02): create IDispatcher interface and WpfDispatcher implementation
    - Add IDispatcher with CheckAccess, Invoke<T>, InvokeAsync(Action), InvokeAsync<T>
    - Add WpfDispatcher sealed class wrapping System.Windows.Threading.Dispatcher
    - ArgumentNullException.ThrowIfNull guard in WpfDispatcher constructor
    - XML documentation on all public members per project standards

---
+++ LogViewer/WpfDispatcher.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace LogViewer
+{
+    /// <summary>
+    /// WPF-specific implementation of <see cref="IDispatcher"/> that delegates to
+    /// <see cref="System.Windows.Threading.Dispatcher"/>.
+    /// </summary>
+    public sealed class WpfDispatcher : IDispatcher
+    {
+        private readonly Dispatcher _dispatcher;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="WpfDispatcher"/> wrapping the specified WPF dispatcher.
+        /// </summary>
+        /// <param name="dispatcher">The WPF dispatcher to wrap. Must not be <see langword="null"/>.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="dispatcher"/> is <see langword="null"/>.</exception>
+        public WpfDispatcher(Dispatcher dispatcher)
+        {
+            ArgumentNullException.ThrowIfNull(dispatcher);
+            _dispatcher = dispatcher;
+        }
+
+        /// <inheritdoc/>
+        public bool CheckAccess() => _dispatcher.CheckAccess();
+
+        /// <inheritdoc/>
+        public T Invoke<T>(Func<T> callback) => _dispatcher.Invoke(callback);
+
+        /// <inheritdoc/>
+        public async Task InvokeAsync(Action callback) => await _dispatcher.InvokeAsync(callback);
+
+        /// <inheritdoc/>
+        public async Task<T> InvokeAsync<T>(Func<T> callback) => await _dispatcher.InvokeAsync(callback);
+    }
+}
